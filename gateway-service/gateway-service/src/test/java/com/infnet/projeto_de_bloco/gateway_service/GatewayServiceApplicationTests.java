4ccfae1da870e267821937d9228cf6538b0d098a
Thalita Policarpo <thalita_policarpo@outlook.com>
2024-09-15 15:38:05 -0300

    gateway-service

---
+++ gateway-service/gateway-service/src/test/java/com/infnet/projeto_de_bloco/gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infnet.projeto_de_bloco.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
