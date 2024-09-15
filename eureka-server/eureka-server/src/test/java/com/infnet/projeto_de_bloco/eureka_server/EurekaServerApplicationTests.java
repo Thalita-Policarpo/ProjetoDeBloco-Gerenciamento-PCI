25d2e3075572353db722bd2685fd811a14d4b1f7
Thalita Policarpo <thalita_policarpo@outlook.com>
2024-09-15 15:37:09 -0300

    Eureka-service

---
+++ eureka-server/eureka-server/src/test/java/com/infnet/projeto_de_bloco/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infnet.projeto_de_bloco.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
